1a1b72466763952094f86b1b8bb542e1c771487f
Jiří Čech <jiricek.cechu@seznam.cz>
2015-09-08 12:59:34 +0200

    new

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Composition;
+using MyContract;
+
+namespace ClassLibrary1
+{
+    [Export(typeof(IMyContract))]
+    public class MyPlugin : IMyContract
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ PluginTest/PluginManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using MyContract;
+
+namespace PluginTest
+{
+    public class PluginManager
+    {
+        [ImportMany]
+        private List<IMyContract> _plugins;
+
+        public void Setup()
+        {
+            _plugins = new List<IMyContract>();
+
+            var aggregate = new System.ComponentModel.Composition.Hosting.AggregateCatalog();
+
+            aggregate.Catalogs.Add(new System.ComponentModel.Composition.Hosting.DirectoryCatalog(
+                "C:\\Plugins\\", "*.dll"));
+            //var directoryCatalog = new DirectoryCatalog(directoryPath, "*.dll");
+            
+            var parts = new System.ComponentModel.Composition.Hosting.CompositionBatch();
+            parts.AddPart(this);
+
+            var container = new System.ComponentModel.Composition.Hosting.CompositionContainer(aggregate);
+            container.Compose(parts);
+        }
+    }
+}
